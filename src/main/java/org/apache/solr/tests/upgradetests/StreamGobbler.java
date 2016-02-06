7558c2783b808c917656d740adb7b8725741735e
Vivek <vivek.narang10@gmail.com>
2016-02-07 01:21:23 +0530

    Update ...

--- src/main/java/org/apache/solr/tests/upgradetests/StreamGobbler.java
+++ src/main/java/org/apache/solr/tests/upgradetests/StreamGobbler.java
@@ -16,15 +16,17 @@ public class StreamGobbler extends Thread {
 	}
 
 	public void run() {
-		try {
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			String line = null;
-			while ((line = br.readLine()) != null)
-				Util.postMessage("  SubProcess: " + type + " >> " + line, MessageType.PROCESS, true);
-
-		} catch (IOException ioe) {
-			Util.postMessage(ioe.getMessage(), MessageType.RESULT_ERRROR, true);
+		synchronized (this) {
+				try {
+					InputStreamReader isr = new InputStreamReader(is);
+					BufferedReader br = new BufferedReader(isr);
+					String line = null;
+					while ((line = br.readLine()) != null)
+						Util.postMessage("  SubProcess: " + type + " >> " + line, MessageType.PROCESS, true);
+		
+				} catch (IOException ioe) {
+					Util.postMessage(ioe.getMessage(), MessageType.RESULT_ERRROR, true);
+				}
 		}
 	}
 
