c25db35cac8d01c4af2c064547b2dfe172362c63
Vivek Narang <vivek.narang10@gmail.com>
2016-02-09 21:29:19 +0530

    Removing extra spaces in the code ...

--- src/main/java/org/apache/solr/tests/upgradetests/StreamGobbler.java
+++ src/main/java/org/apache/solr/tests/upgradetests/StreamGobbler.java
@@ -11,21 +11,21 @@ public class StreamGobbler extends Thread {
 	String type;
 
 	StreamGobbler(InputStream is, String type) {
-		this.is = is;
-		this.type = type;
+			this.is = is;
+			this.type = type;
 	}
 
 	public void run() {
-				try {
-					InputStreamReader isr = new InputStreamReader(is);
-					BufferedReader br = new BufferedReader(isr);
-					String line = null;
-					while ((line = br.readLine()) != null)
-						Util.postMessage("  SubProcess: " + type + " >> " + line, MessageType.PROCESS, true);
-		
-				} catch (IOException ioe) {
-					Util.postMessage(ioe.getMessage(), MessageType.RESULT_ERRROR, true);
-				}
+			try {
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String line = null;
+				while ((line = br.readLine()) != null)
+					Util.postMessage("  SubProcess: " + type + " >> " + line, MessageType.PROCESS, true);
+	
+			} catch (IOException ioe) {
+				Util.postMessage(ioe.getMessage(), MessageType.RESULT_ERRROR, true);
+			}
 	}
 
 }
